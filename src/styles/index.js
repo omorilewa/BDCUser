312e06540a78db1778fce566b585fed5fbfc1323
omorilewa <mariam3105@gmail.com>
2018-05-24 12:08:21 +0100

    add styles

--- src/styles/index.js
+++ src/styles/index.js
@@ -45,3 +45,22 @@ export const StubStyles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+export const RateScreenStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+  imagebg: {
+    width: '100%',
+    height: '100%',
+  },
+  currView: {
+    width: '100%',
+    height: '80%'
+  },
+  cut: {
+    width: '100%'
+  }
+})
