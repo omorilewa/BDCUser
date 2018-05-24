b3a3dd654d7ff6df8b57225269b53fd0094a0286
omorilewa <mariam3105@gmail.com>
2018-05-24 12:08:02 +0100

    add rates display

---
+++ src/components/RatesDisplay.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+
+export default class Home extends Component {
+  static propTypes = {
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Hello from the other side</Text>
+      </View>
+    );
+  }
+}
