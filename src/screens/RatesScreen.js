ff09af36cf3a879652a462d247d4956d279d8d55
omorilewa <mariam3105@gmail.com>
2018-06-07 23:57:54 +0100

    add Title to rate screen

--- src/screens/RatesScreen.js
+++ src/screens/RatesScreen.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import { Rates } from '../components';
 
 class RatesScreen extends Component {
+  static navigationOptions = {
+    title: 'Rates',
+    headerTitleStyle: {
+      fontFamily: 'OpenSans-Regular',
+      color: '#FFFC00'
+    },
+  };
   static displayName = 'RatesScreen';
 
   render() {
