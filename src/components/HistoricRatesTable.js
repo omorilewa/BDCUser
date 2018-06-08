c3775350e3736d234c5995f5316ad086ddab7f77
oreoluwade <oreoluwade@gmail.com>
2018-06-08 14:23:30 +0100

    Create HistoricRatesTable component to be in Tab of TabNav

---
+++ src/components/HistoricRatesTable.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import ModalDropdown from 'react-native-modal-dropdown';
+import { HistoricRatesStyles as styles } from '../styles';
+
+export default class HistoricRatesTable extends Component {
+  static propTypes = {
+  }
+
+  state = {
+    ratesData: []
+  }
+
+  onClickDropDown = () => {
+    console.log('clicked');
+    this.setState(() => ({
+      ratesData: []
+    }));
+  }
+
+  render() {
+    return (
+      <View style={styles.filterView}>
+        <Text style={styles.filterText}>Showing: </Text>
+        <TouchableOpacity onPress={() => { this.dropDown && this.dropDown.show(); }}>
+          <View style={styles.modalView}>
+            <ModalDropdown ref={(el) => { this.dropDown = el; }}
+              options={['All time', 'Last 7 days', 'Today', 'This month']}
+              defaultValue={'All time'}
+              style={styles.modal}
+              textStyle={styles.buttonText}
+              dropdownStyle={styles.dropdown}
+              dropdownTextStyle={styles.dropdowntext}
+              adjustFrame={(style) => {
+                style.height = 88;
+                style.top = 144;
+                return style;
+              }}
+              onSelect={() => { this.onClickDropDown(); }}
+            />
+            <Text style={styles.arrow}>▼</Text>
+          </View>
+          <View style={styles.hr}></View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
