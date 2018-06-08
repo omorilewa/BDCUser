989e98e06dcb31f930b36ccc482c7de1a0d8caf0
oreoluwade <oreoluwade@gmail.com>
2018-06-08 16:04:12 +0100

    remove duplicated code

--- android/app/src/main/java/com/bdcuser/MainApplication.java
+++ android/app/src/main/java/com/bdcuser/MainApplication.java
@@ -3,7 +3,6 @@ package com.bdcuser;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new SplashScreenReactPackage(),
             new VectorIconsPackage(),
             new SplashScreenReactPackage()
       );
