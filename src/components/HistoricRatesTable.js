17b42cb8e82cc45d49fa2625aa3f1f85263d615c
oreoluwade <oreoluwade@gmail.com>
2018-06-08 15:44:49 +0100

    mount HistoricRatesHeader in HistoricRatesTable component

--- src/components/HistoricRatesTable.js
+++ src/components/HistoricRatesTable.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import ModalDropdown from 'react-native-modal-dropdown';
 import { HistoricRatesStyles as styles } from '../styles';
+import { HistoricRatesHeader } from '.';
 
 export default class HistoricRatesTable extends Component {
   static propTypes = {
@@ -20,29 +21,32 @@ export default class HistoricRatesTable extends Component {
 
   render() {
     return (
-      <View style={styles.filterView}>
-        <Text style={styles.filterText}>Showing: </Text>
-        <TouchableOpacity onPress={() => { this.dropDown && this.dropDown.show(); }}>
-          <View style={styles.modalView}>
-            <ModalDropdown ref={(el) => { this.dropDown = el; }}
-              options={['All time', 'Last 7 days', 'Today', 'This month']}
-              defaultValue={'All time'}
-              style={styles.modal}
-              textStyle={styles.buttonText}
-              dropdownStyle={styles.dropdown}
-              dropdownTextStyle={styles.dropdowntext}
-              adjustFrame={(style) => {
-                style.height = 88;
-                style.top = 144;
-                return style;
-              }}
-              onSelect={() => { this.onClickDropDown(); }}
-            />
-            <Text style={styles.arrow}>▼</Text>
-          </View>
-          <View style={styles.hr}></View>
-        </TouchableOpacity>
-      </View>
+      <Fragment>
+        <View style={styles.filterView}>
+          <Text style={styles.filterText}>Showing: </Text>
+          <TouchableOpacity onPress={() => { this.dropDown && this.dropDown.show(); }}>
+            <View style={styles.modalView}>
+              <ModalDropdown ref={(el) => { this.dropDown = el; }}
+                options={['All time', 'Last 7 days', 'Today', 'This month']}
+                defaultValue={'All time'}
+                style={styles.modal}
+                textStyle={styles.buttonText}
+                dropdownStyle={styles.dropdown}
+                dropdownTextStyle={styles.dropdowntext}
+                adjustFrame={(style) => {
+                  style.height = 88;
+                  style.top = 144;
+                  return style;
+                }}
+                onSelect={() => { this.onClickDropDown(); }}
+              />
+              <Text style={styles.arrow}>▼</Text>
+            </View>
+            <View style={styles.hr}></View>
+          </TouchableOpacity>
+        </View>
+        <HistoricRatesHeader />
+      </Fragment>
     );
   }
 }
