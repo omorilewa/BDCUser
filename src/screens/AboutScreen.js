a03e30fa0640d8a8d37318a97d0f4a82d1c98466
oreoluwade <oreoluwade@gmail.com>
2018-06-08 12:00:51 +0100

    add title to AboutScreen

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -3,6 +3,13 @@ import { About } from '../components';
 
 class AboutScreen extends Component {
   static displayName = 'AboutScreen';
+  static navigationOptions = {
+    title: 'About ABCON',
+    headerTitleStyle: {
+      fontFamily: 'OpenSans-Regular',
+      color: '#FFFC00'
+    },
+  };
 
   render() {
     return (
