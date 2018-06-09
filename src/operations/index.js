8db4a0ff2e7cc1c3a4b80e4803058c7664ca19b9
oreoluwade <oreoluwade@gmail.com>
2018-06-09 11:46:12 +0100

    add computedRates operation for Lagos

--- src/operations/index.js
+++ src/operations/index.js
@@ -9,3 +9,14 @@ export const GET_LOCATION = gql`
   }
 `;
 
+// Computed rates for Lagos alone
+export const GET_COMPUTED_RATES = gql`
+  query computedRates {
+    computedRates(first: 100, locationId: "cjhgeu3rh0004f2qu4cfk2my8") {
+      date
+      currency
+      buyRate
+      sellRate
+    }
+  }
+`;
