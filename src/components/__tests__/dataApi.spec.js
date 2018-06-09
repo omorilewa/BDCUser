f4868ab049b0f4809c4c27bed633aa7569815db8
omorilewa <mariam3105@gmail.com>
2018-06-09 16:34:30 +0100

    increase coverage

--- src/components/__tests__/dataApi.spec.js
+++ src/components/__tests__/dataApi.spec.js
@@ -1,4 +1,4 @@
-import { findLocationById } from '../../dataApi';
+import { findLocationById, sortTodayRates } from '../../dataApi';
 
 const locations = [
   {
@@ -23,6 +23,31 @@ const locations = [
   }
 ];
 
+const data = {
+  'todaysRates': [
+    {
+      'buyRate': 359,
+      'sellRate': 360,
+      'currency': 'USD',
+      'periodOfTheDay': 'MORNING',
+      'date': '2018-06-09',
+      'location': {
+        'name': 'Lagos'
+      }
+    },
+    {
+      'buyRate': 490,
+      'sellRate': 495,
+      'currency': 'GBP',
+      'periodOfTheDay': 'MORNING',
+      'date': '2018-06-09',
+      'location': {
+        'name': 'Lagos'
+      }
+    }
+  ]
+};
+
 describe('Finding Locations by their IDs', () => {
   it('returns the name of the Location, given the ID', () => {
     const result = findLocationById(locations, 'cjhgeu3rh0008f2quhkuh7vpv');
@@ -32,5 +57,12 @@ describe('Finding Locations by their IDs', () => {
   it('returns a descriptive message when ID is not valid', () => {
     const result = findLocationById(locations, 'haijiqwidjojdlkklsdf');
     expect(result).toEqual('Location not available');
+  });
+});
+
+describe('sort location data', () => {
+  it('returns the sorted data, given a data imput', () => {
+    const result = sortTodayRates(data);
+    expect(result).toBeTruthy;
   });
 });
