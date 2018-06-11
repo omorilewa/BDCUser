2c46515af3b2f7d3d6cb61192ec5fdae4ca84e1f
omorilewa <mariam3105@gmail.com>
2018-06-11 08:01:13 +0100

    Reduce triple map to single map - Allow scrollable view - Update designs

--- src/components/HistoricRatesItem.js
+++ src/components/HistoricRatesItem.js
@@ -1,10 +1,10 @@
-import React, { Component, Fragment } from 'react';
+import React, { PureComponent } from 'react';
 import { View, Text, Image } from 'react-native';
 import moment from 'moment';
 import { HistoricRatesStyles as styles } from '../styles';
 import { getImageFromCurrency, trimRates } from '../dataApi';
 
-export default class HistoricRatesItem extends Component {
+export default class HistoricRatesItem extends PureComponent {
   state = {
     ratesPerDate: this.props.ratesPerDate,
     date: this.props.date
@@ -25,33 +25,27 @@ export default class HistoricRatesItem extends Component {
     const formattedDate = moment(date).format('ddd, MMM D');
 
     return (
-      <Fragment>
-        <View style={styles.bodyRowView}>
-          <View style={styles.dateColumn}>
-            <Text style={styles.dateText}>{formattedDate}</Text>
-          </View>
-          <Fragment>
-            <View style={styles.currDataView}>
-              {trimmedRates.map((rate, index) => (
-                <View style={styles.currItemView} key={index}>
-                  <Image source={getImageFromCurrency(rate.currency)} style={styles.currImage}/>
-                  <Text style={styles.ratesText}>{rate.currency}</Text>
-                </View>
-              ))}
-            </View>
-            <View style={styles.ratesItemView}>
-              {trimmedRates.map((rate, index) => (
-                <Text key={index} style={styles.ratesText}>{rate.buyRate}</Text>
-              ))}
-            </View>
-            <View style={styles.ratesItemView}>
-              {trimmedRates.map((rate, index) => (
-                <Text key={index} style={styles.ratesText}>{rate.sellRate}</Text>
-              ))}
-            </View>
-          </Fragment>
+      <View style={styles.bodyRowView}>
+        <View style={styles.dateColumn}>
+          <Text style={styles.dateText}>{formattedDate}</Text>
         </View>
-      </Fragment>
+        <View style={styles.bottomMargin}>
+          {trimmedRates.map((rate, index) =>
+            <View style={styles.bodyRowView} key={index}>
+              <View style={styles.currItemView}>
+                <Image source={getImageFromCurrency(rate.currency)} style={styles.currImage} />
+                <Text style={styles.ratesText}>{rate.currency}</Text>
+              </View>
+              <View style={styles.ratesItemView}>
+                <Text key={index} style={styles.ratesText}>{rate.buyRate}</Text>
+              </View >
+              <View style={styles.ratesItemView}>
+                <Text key={index} style={styles.ratesText}>{rate.sellRate}</Text>
+              </View>
+            </View>
+          )}
+        </View>
+      </View>
     );
   }
 }
